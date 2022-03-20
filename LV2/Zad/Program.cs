e531e7e7c3eefbe6d2217b54bf1c51d2271165d1
janaledencan <janaledencan@gmail.com>
2022-03-20 14:11:13 +0100

    Initial commit

---
+++ LV2/Zad/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//1.zad Kreirajte objekt klase DiceRoller i u njega ubacite 20 kockica. Baciti sve kockice i ispisati rezultate bacanja kockica na ekran.
+namespace Zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            const int numberOfDice= 20;
+            DiceRoller diceRoller = new DiceRoller();
+            
+            int i;
+            for(i=0; i < numberOfDice; i++)
+            {
+                diceRoller.InsertDie(new Die(6));
+            }
+
+            diceRoller.RollAllDice();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            IList<int> rollingResults = diceRoller.GetRollingResults();
+
+            foreach(int rollingResult in rollingResults)
+            {
+                stringBuilder.Append(rollingResult.ToString()+ ", ");
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+            
+
+        }
+    }
+}
