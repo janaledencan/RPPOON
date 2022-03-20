ae573436258c212d316163da0415490ddcae145e
janaledencan <janaledencan@gmail.com>
2022-03-20 19:27:53 +0100

    Task 6 without changes

---
+++ LV2/Zad/IDiceRoller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zad
+{
+    internal interface IDiceRollercs
+    {
+        void InsertDie();
+        void RemoveAllDice();
+        void RollAllDice();
+    }
+}
